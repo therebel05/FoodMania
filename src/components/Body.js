005ec2cfdab418c2b8c0c46312a9c7f828202c70
Akshay Raut <rautakshay78@gmail.com>
2025-11-27 14:14:45 +0530

    Search functionality for rstaurant name

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { resData } from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
   const [searchText, setSearchText] = useState("");
-  const [restaurantList, setRestaurantList] = useState(resData);
+  const [restaurantList, setRestaurantList] = useState([]);
+  const [filteredRestaurantList, setFilteredRestaurantList] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.2018083&lng=85.79357139999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const jsonData = await data.json();
+
+    const restaurants =
+      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+
+    const restaurantInfo = restaurants.map((r) => r.info);
+
+    setRestaurantList(restaurantInfo);
+    setFilteredRestaurantList(restaurantInfo);
+  };
 
   const handleSearch = () => {
-    const filteredList = resData.filter((res) =>
+    const filteredList = restaurantList.filter((res) =>
       res.name.toLowerCase().includes(searchText.toLowerCase())
     );
-    setRestaurantList(filteredList);
+    setFilteredRestaurantList(filteredList);
   };
 
   return (
@@ -26,7 +47,7 @@ const Body = () => {
         <button onClick={handleSearch}>Search</button>
       </div>
       <div className="res-container">
-        {restaurantList.map((restaurant) => (
+        {filteredRestaurantList.map((restaurant) => (
           <RestaurantCard key={restaurant.id} {...restaurant} />
         ))}
       </div>
